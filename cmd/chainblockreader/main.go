83ac22a7bc5595509b5bc44ad6790841b7e4ec0a
Edine <152820927+HoussemDhiaEddine19@users.noreply.github.com>
2025-12-12 06:58:25 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/chainblockreader/main.go
@@ -0,0 +1,23 @@
+// cmd/chainblockreader/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "chainblockreader/internal/chainblockreader"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := chainblockreader.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
